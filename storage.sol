8791277f2b0c2b02ddc419d2c94fdbd475d036eb
yumeinaruu <stanislav.lisovoj@krainet.by>
2025-12-19 15:12:31 +0300

    structs and arrays

--- storage.sol
+++ storage.sol
@@ -2,9 +2,34 @@
 pragma solidity 0.8.30;
 
 contract Storage {
-    uint256 public favouriteNumber;
+    uint256 myFavouriteNumber;
 
-    function store(uint256 _favouriteNumber) public {
-        favouriteNumber = _favouriteNumber;
+    //uint256[] listOfVavoriteNumbers;
+
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // Person public myFriend = Person(7, "Alo");
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    // static array
+    Person[3] public listOfPeopleStatic;
+
+
+    function store(uint256 _myFavouriteNumber) public {
+        myFavouriteNumber = _myFavouriteNumber;
+    }
+
+    // pure, view
+    function retrieve() public view returns(uint256) {
+        return myFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name));
     }
 }
