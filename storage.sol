39807a0cec913502216bf049b654d794838e8ddc
yumeinaruu <stanislav.lisovoj@krainet.by>
2025-12-19 16:37:40 +0300

    add mapping and some comments

--- storage.sol
+++ storage.sol
@@ -17,19 +17,25 @@ contract Storage {
     Person[] public listOfPeople;
 
     // static array
-    Person[3] public listOfPeopleStatic;
+    //Person[3] public listOfPeopleStatic;
 
+    mapping(string => uint256) public nameToFavouriteNumber;
 
     function store(uint256 _myFavouriteNumber) public {
         myFavouriteNumber = _myFavouriteNumber;
     }
 
-    // pure, view
+    // pure canot read or change contract variables, only for local func vars
+    // view can read contract variables but cannot change them
     function retrieve() public view returns(uint256) {
         return myFavouriteNumber;
     }
 
+    // calldata, memory, storage
+    // memory - only exists for the duration of the function call. read and write
+    // calldata - temporarily as memory but only read-only. takes less gas
     function addPerson(string memory _name, uint256 _favouriteNumber) public {
         listOfPeople.push(Person(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
     }
 }
